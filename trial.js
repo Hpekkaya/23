3bc6e2d432c05a3161771d0677ab6de0d87b1e4a
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-14 19:46:34 +0300

    map function tried

--- trial.js
+++ trial.js
@@ -1,12 +1,4 @@
 
-// itaration(döngüler)
-const numbers = [45,5,6,55,77]
 
-numbers.forEach(myFunction);
-console.log(numbers)
-function myFunction(value,index,array){
-        numbers[index] *= 2
-        console.log(index + "-" + value)
-        // return value * 2 ;
-    }
+
 
