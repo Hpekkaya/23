8464ed7f3919e1a516ce32e59392148dc952fd6b
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-14 18:06:33 +0300

    forEach iteration tried

--- trial.js
+++ trial.js
@@ -1,3 +1,12 @@
 
+// itaration(döngüler)
+const numbers = [45,5,6,55,77]
 
+numbers.forEach(myFunction);
+console.log(numbers)
+function myFunction(value,index,array){
+        numbers[index] *= 2
+        console.log(index + "-" + value)
+        // return value * 2 ;
+    }
 
