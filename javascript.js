2b14695c83da1f01faebf72b639d42462e17ea95
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-14 10:33:05 +0300

    slice and splice tried

---
+++ javascript.js
@@ -0,0 +1,78 @@
+// let x = "10"
+// let y = 20
+
+// console.log("typeof y", typeof y )
+// console.log("typeof y", typeof  x)
+// console.log("After implying funcion 'toString' typeof y is", typeof y.toString())
+// console.log("After implying funcion 'Number'   typeof y is", typeof Number(x))
+
+
+// let a = 9
+// console.log(a.toFixed(2))
+
+// let z=2.2555
+
+// console.log(z.toFixed(0))
+// console.log(z.toFixed(1))
+// console.log(z.toFixed(2))
+// console.log(z.toFixed(3))
+// console.log(z.toFixed(4))
+
+// //array
+
+// const cars = ["fiat","volvo","opel"]
+// console.log(cars[0])
+// console.log(cars)
+// console.log(cars[cars.length-1])
+// console.log(cars.toString().split(","))
+
+// //array
+
+// const cars = ["fiat","volvo","opel"]
+// console.log(cars[0])
+// console.log(cars)
+// console.log(cars[cars.length-1])
+// console.log(cars.length)
+// console.log(cars.toString().split(","))
+
+//sondan çıkarma işlemi
+// const cars = ["fiat","volvo","opel"]
+
+// console.log(cars)
+// let value = cars.pop()
+// console.log(cars)
+// cars.pop()
+// console.log(cars)
+// console.log(value)
+// let pushing = cars.push("BMW")
+// console.log(pushing)
+// console.log(cars)
+// let value2 = cars.shift()
+// console.log(value2)
+// console.log(cars, value2)
+// let value3 = cars.unshift("volkswagen","ferrari")
+// console.log(cars, value3)
+// console.log(cars.toString())
+// console.log(cars.join(" / "))
+// console.log(cars.join(""))
+// const cars2 = ["car1","car2","car3"]
+// const allCars = cars2.concat(cars2,["car4"])
+// console.log(allCars)
+// const meyveler = ["elma","armut","mandalina"]
+// console.log(meyveler)
+// // splice() :Adds/Removes elements from an array
+// meyveler.splice(0,3,"çilek","ayva","muz")
+
+// // slice()	Selects a part of an array, 
+// // and returns the new array
+// console.log("Yeni Meyveler:", meyveler)
+// let secmeMeyveler = meyveler.slice(-1)
+// console.log("Yeni Meyveler:", meyveler)
+
+// let secmeMeyveler2 = meyveler.slice(1,4)
+// console.log("Yeni Seçme Meyveler:",secmeMeyveler)
+// console.log("Yeni Seçme Meyveler-2:",secmeMeyveler2)
+
+
+
+
--- trial.js
+++ trial.js
@@ -1,15 +1,3 @@
 
-//sondan çıkarma işlemi
-const cars = ["fiat","volvo","opel"]
-
-console.log(cars)
-let value = cars.pop()
-console.log(cars)
-cars.pop()
-console.log(cars)
-console.log(value)
-let pushing = cars.push("BMW")
-console.log(cars , pushing)
-
-
+// 
 
