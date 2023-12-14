53fba61024dbafd6796a4cf01d2982ab9bd7e337
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-14 17:34:09 +0300

    Sort function tried

--- trial.js
+++ trial.js
@@ -1,3 +1,7 @@
 
-// 
+
+const cars = [{type:"Volvo", year:2020},{type:"Fiat", year:2015},{type:"BMW", year:2010}]
+console.log(cars)
+// const carsNew = cars.sort(function(a,b){return a.year-b.year})
+// console.log(carsNew)
 
